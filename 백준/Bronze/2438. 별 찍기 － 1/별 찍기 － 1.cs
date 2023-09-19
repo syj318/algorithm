38085342fb1003c6679fca0220485c65eac24755
yoojin <76746507+syj318@users.noreply.github.com>
2023-09-19 18:15:10 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 328 ms, Memory: 6576 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace for문9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
